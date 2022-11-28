5ca01b0fd8876a5774419e36c1bcc23ba892361c
muradxkira <mahfujur797@gmail.com>
2022-11-28 23:36:10 +0600

    key hidden

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,20 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_apiKey,
-//   authDomain: process.env.REACT_APP_authDomain,
-//   projectId: process.env.REACT_APP_projectId,
-//   storageBucket: process.env.REACT_APP_storageBucket,
-//   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-//   appId: process.env.REACT_APP_appId
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
@@ -22,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDuWj1vjge0ZEGoYDT17U6dynda-JUA5zM",
-  authDomain: "recyclelib.firebaseapp.com",
-  projectId: "recyclelib",
-  storageBucket: "recyclelib.appspot.com",
-  messagingSenderId: "764455877980",
-  appId: "1:764455877980:web:2152ebd142e4f1e33f1599"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
